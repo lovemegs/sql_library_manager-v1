eb3581ffe8a99330b6699b210070d2d16d26f24c
Megan Love <lovemegs94@hotmail.com>
2022-12-11 23:26:09 -0700

    Finished product

--- app.js
+++ app.js
@@ -38,40 +38,23 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-
-
-
 // catch 404 and foward to error handler
-// app.use((req, res, next) => {
-//   const err = new Error();
-//   err.status = 404;
-//   err.message = 'Sorry, the route could not be found';
-//   res.render();
-//   next(err);
-// });
-// // GLOBAL ERROR HANDLER
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   console.log();
-//   // res.render('error', {});
-// });
+app.use((req, res, next) => {
+  const err = new Error();
+  err.status = 404;
+  err.message = 'Sorry, the route could not be found';
+  res.render('page-not-found', { error });
+  next(err);
+});
+// GLOBAL ERROR HANDLER
+app.use((err, req, res, next) => {
+  if (err.status !== 404) {
+    err.status = 500;
+    err.message = 'Something went wrong on the server';
+    console.log(err.message, err.status);
+    res.render('error', { err });
+  }
+});
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -26,31 +26,70 @@ router.get('/books', asyncHandler(async (req, res) => {
     res.render('index', { books, title: 'Books' });
 }));
 
-// GET create book
+
+// GET create a book form
 router.get('/books/new', asyncHandler(async (req, res) => {
   res.render('new-book', { book: {}, title: 'New Book' });
 }));
-// POST create book
+// POST a new book to the database
 router.post('/books/new', asyncHandler(async (req, res) => {
-  const book = await Book.create(req.body);
-  res.redirect('/books' + book.id);
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect('/books');
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      book = await Book.build(req.body);
+      res.render('new-book', { book, errors: error.errors, title: 'New Book' });
+    } else {
+      throw error;
+    }
+  }
 }));
 
 
-// GET book id
+// GET book detail form
 router.get('/books/:id', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
-  res.render('update-book', { book, title: 'Update Book' })
+  if (book) {
+    res.render('update-book', { book, title: 'Update Book' })
+  } else {
+    res.status(404);
+  }
 }));
-// POST book id
+// POST updated book info in the database
 router.post('/books/:id', asyncHandler(async (req, res) => {
-
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.update(req.body);
+      res.redirect('/books');
+    } else {
+      res.status(404);
+    }
+  } catch (error) {
+      if (error.name === 'SequelizeValidationError') {
+        book = await Book.build(req.body);
+        book.id = req.params.id;
+        res.render('update-book', { book, errors: error.errors, title: 'New Book' });
+      } else {
+        throw error;
+      }
+  }
 }));
 
-// // POST delete book
-// router.post(() => {
 
-// })
+// POST delete book
+router.post('/books/:id/delete', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    await book.destroy();
+    res.redirect('/books');
+  } else {
+    res.status(404);
+  }
+}));
 
 
 module.exports = router;
